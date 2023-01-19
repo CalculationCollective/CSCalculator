3ae63e16d902d9239af8964a7209d9e9b808153a
Teddy <teddy@teddy.email>
2023-01-19 17:24:43 +0100

    add Calculator

---
+++ CSCalculator.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Enter the first number: ");
+var num1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Choose between:\n1) Addition\n2) Subtraction\n3) Multiplication\n4) Division");
+var calculationType = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+var num2 = double.Parse(Console.ReadLine());
+
+switch (calculationType)
+{
+    case 1:
+        Console.Write(num1 + num2);
+        break;
+    case 2:
+        Console.Write(num1 - num2);
+        break;
+    case 3:
+        Console.Write(num1 * num2);
+        break;
+    case 4:
+        Console.Write(num1 / num2);
+        break;
+    default:
+        Console.Write($"{calculationType} is not a valid function");
+        break;
+}
